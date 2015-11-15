b9e384b4aa4b05f37aa501ea56e96481919cc54e
Troy SK <troy@troysk.com>
2015-11-15 19:55:31 +0530

    Increase font timeout more

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,7 +6,7 @@ Licensed MIT
 (function(w){ "use strict"; var loadCSS = function( href, before, media ){ var doc = w.document; var ss = doc.createElement( "link" ); var ref; if( before ){ ref = before; } else { var refs = ( doc.body || doc.getElementsByTagName( "head" )[ 0 ] ).childNodes; ref = refs[ refs.length - 1]; } var sheets = doc.styleSheets; ss.rel = "stylesheet"; ss.href = href; ss.media = "only x"; ref.parentNode.insertBefore( ss, ( before ? ref : ref.nextSibling ) ); var onloadcssdefined = function( cb ){ var resolvedHref = ss.href; var i = sheets.length; while( i-- ){ if( sheets[ i ].href === resolvedHref ){ return cb(); } } setTimeout(function() { onloadcssdefined( cb ); }); }; ss.onloadcssdefined = onloadcssdefined; onloadcssdefined(function() { ss.media = media || "all"; }); return ss; }; if( typeof module !== "undefined" ){ module.exports = loadCSS; } else { w.loadCSS = loadCSS; } }( typeof global !== "undefined" ? global : this ));
 loadCSS("/stylesheets/style.css");
 var observer = new FontFaceObserver("komika_displayregular", {});
-observer.check('Hii', 5000).then(function () {
+observer.check('Hii', 10000).then(function () {
   var body = document.getElementsByTagName("body")[0];
   body.className = body.className + " fonts-loaded";
 });
