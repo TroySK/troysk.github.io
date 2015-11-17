5a6cda7ff1f0010b62afaf21340023618614760b
Troy SK <troy@troysk.com>
2015-11-17 13:47:27 +0530

    Move GA to main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -195,3 +195,9 @@ observer.check('Hii', 10000).then(function () {
 })();
 loadCSS("//fonts.googleapis.com/css?family=Roboto:100");
 loadCSS("//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css");
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-70129298-1', 'auto');
+ga('send', 'pageview');
